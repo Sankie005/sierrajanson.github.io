843ea632882be6872e31d02bd1420073e2211bcf
Sierra Janson <60485703+sierrajanson@users.noreply.github.com>
2023-12-23 23:58:22 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ const observer = new IntersectionObserver(entries => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach(element => observer.observe(element));
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", DisplayContent);
+DisplayContent();
+
+function DisplayContent() {
+  const TriggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    const topBox = box.getBoundingClientRect().top;
+
+    if (topBox < TriggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
